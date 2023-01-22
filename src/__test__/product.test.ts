673b54558dadc10c55eb15e931717091e48f79c1
abdo-355 <abdoluxor366@gmail.com>
2023-01-23 00:40:29 +0200

    add create product tests

--- src/__test__/product.test.ts
+++ src/__test__/product.test.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 
 import app from "../app";
 import { createProduct } from "../service/product.service";
+import { signJwt } from "../utils/jwt.utils";
 
 const userId = new mongoose.Types.ObjectId().toString();
 
@@ -14,6 +15,12 @@ const productPayload = {
     "Designed for first-time DSLR owners who want impressive results straight out of the box, capture those magic moments no matter your level with the EOS 1500D. With easy to use automatic shooting modes, large 24.1 MP sensor, Canon Camera Connect app integration and built-in feature guide, EOS 1500D is always ready to go.",
   price: 879.99,
   image: "https://i.imgur.com/QlRphfQ.jpg",
+};
+
+const userPayload = {
+  _id: userId,
+  email: "j ane.doe@exampte.com",
+  name: "Jane Doe",
 };
 
 describe("product", () => {
@@ -43,6 +50,26 @@ describe("product", () => {
 
       expect(statusCode).toBe(200);
       expect(body.productId).toBe(product.productId);
+    });
+  });
+
+  describe("create product route", () => {
+    it("should return a 403 if the user isn't signed in", async () => {
+      const { statusCode } = await supertest(app).post("/api/products");
+
+      expect(statusCode).toBe(403);
+    });
+
+    it("should return a 201 if the user is signed in", async () => {
+      //@ts-ignore
+      const jwt = signJwt(userPayload);
+
+      const { statusCode } = await supertest(app)
+        .post("/api/products")
+        .set("Authorization", `Bearer ${jwt}`)
+        .send(productPayload);
+
+      expect(statusCode).toBe(201);
     });
   });
 });
